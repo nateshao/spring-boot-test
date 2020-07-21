04e769b31f3520e963086a89905512235e7ed599
nateshao <shaotongjie@gmail.com>
2020-07-21 23:01:01 +0800

    新建boot-03-actuator

---
+++ nateshao-boot-03-actuator/src/main/java/com/nateshao/config/CustomEndpoint.java
@@ -0,0 +1,23 @@
+package com.nateshao.config;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+/**
+ * @date Created by 邵桐杰 on 2020/7/21 22:58
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ */
+@Component
+@Endpoint(id = "customEndpoint")
+public class CustomEndpoint {
+
+    @ReadOperation
+    public String custome(){
+        return "hello nateshao!!";
+    }
+}
+
