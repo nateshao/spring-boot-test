472fbc4a3fe058a0918399f1b245b60288d7e97e
nateshao <shaotongjie@gmail.com>
2020-07-21 22:27:19 +0800

    新建boot-03-actuator

---
+++ nateshao-boot-03-actuator/src/main/java/com/nateshao/NateshaoBootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBootActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NateshaoBootActuatorApplication.class, args);
+	}
+
+}
