cc8690999e1171e64e9d7f924882b9b4dcb0c81b
nateshao <shaotongjie@gmail.com>
2020-07-25 11:03:46 +0800

    初始化nateshao-boot-05-logback完成

---
+++ nateshao-boot-05-logback/src/main/java/com/nateshao/NateshaoBoot05LogbackApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot05LogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot05LogbackApplication.class, args);
+    }
+
+}
