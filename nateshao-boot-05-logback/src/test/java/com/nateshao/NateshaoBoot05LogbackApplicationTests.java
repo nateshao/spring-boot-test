f1a0630dede94b61b37210f1f974f17a2efada26
nateshao <shaotongjie@gmail.com>
2020-07-25 12:51:42 +0800

    初始化 添加app.log

--- nateshao-boot-05-logback/src/test/java/com/nateshao/NateshaoBoot05LogbackApplicationTests.java
+++ nateshao-boot-05-logback/src/test/java/com/nateshao/NateshaoBoot05LogbackApplicationTests.java
@@ -1,13 +1,18 @@
 package com.nateshao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class NateshaoBoot05LogbackApplicationTests {
 
     @Test
     void contextLoads() {
+        for (int i = 0; i < 10; i++) {
+            log.info("info 级别日志文件输出");
+        }
     }
 
 }
