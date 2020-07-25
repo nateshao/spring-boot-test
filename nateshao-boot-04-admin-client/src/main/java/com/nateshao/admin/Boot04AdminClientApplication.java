735a58d746c8e0f4e36f74507b1a677f016e9350
nateshao <shaotongjie@gmail.com>
2020-07-25 10:50:40 +0800

    nateshao-boot-04-admin-client 完成

---
+++ nateshao-boot-04-admin-client/src/main/java/com/nateshao/admin/Boot04AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot04AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot04AdminClientApplication.class, args);
+    }
+
+}
---
+++ nateshao-boot-04-admin-client/src/test/java/com/nateshao/admin/Boot04AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot04AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
