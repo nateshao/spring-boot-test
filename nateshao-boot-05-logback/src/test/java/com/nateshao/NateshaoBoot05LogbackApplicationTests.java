78147102618b8b1220015239bc37192cdd2281db
nateshao <shaotongjie@gmail.com>
2020-07-25 13:45:04 +0800

    finish

--- nateshao-boot-05-logback/src/test/java/com/nateshao/NateshaoBoot05LogbackApplicationTests.java
+++ nateshao-boot-05-logback/src/test/java/com/nateshao/NateshaoBoot05LogbackApplicationTests.java
@@ -2,17 +2,56 @@ package com.nateshao;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
 @SpringBootTest
 class NateshaoBoot05LogbackApplicationTests {
 
+
     @Test
     void contextLoads() {
-        for (int i = 0; i < 10; i++) {
+        MDC.put("user_id","1000");
+
+        for (int i = 0; i < 1000; i++) {
             log.info("info 级别日志文件输出");
         }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
