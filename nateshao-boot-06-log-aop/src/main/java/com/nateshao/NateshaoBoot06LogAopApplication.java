7248245311e841932918339f4d843a265d3e982d
nateshao <shaotongjie@gmail.com>
2020-07-29 23:59:59 +0800

    初始化nateshao-boot-06-log-aop

---
+++ nateshao-boot-06-log-aop/src/main/java/com/nateshao/NateshaoBoot06LogAopApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot06LogAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot06LogAopApplication.class, args);
+    }
+
+}
