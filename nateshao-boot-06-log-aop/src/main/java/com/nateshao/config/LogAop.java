81ef0e5c761d63f32551810df3bc614c05586ea9
nateshao <shaotongjie@gmail.com>
2020-07-30 00:25:34 +0800

    add AOP

--- nateshao-boot-06-log-aop/src/main/java/com/nateshao/config/LogAop.java
+++ nateshao-boot-06-log-aop/src/main/java/com/nateshao/config/LogAop.java
@@ -1,5 +1,10 @@
 package com.nateshao.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
 /**
  * @date Created by 邵桐杰 on 2020/7/30 0:18
  * @微信公众号 千羽的编程时光
@@ -7,6 +12,15 @@ package com.nateshao.config;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  */
+@Slf4j
+@Component
+@Aspect
 public class LogAop {
+
+    @Around("execution(public * com.nateshao.controller.*Controller.*(..))")
+    public Object around(){
+
+        return "";
+    }
 
 }
