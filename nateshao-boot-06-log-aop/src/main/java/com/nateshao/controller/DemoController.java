8c9383fbedc993006f3fe5473dc6b1d3018cc554
nateshao <shaotongjie@gmail.com>
2020-07-30 00:17:11 +0800

    add DemoController

---
+++ nateshao-boot-06-log-aop/src/main/java/com/nateshao/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.nateshao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @date Created by 邵桐杰 on 2020/7/30 0:11
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ */
+@ResponseBody
+public class DemoController {
+    @GetMapping
+    public String demo(String username){
+        return "hello " + username;
+    }
+
+}
