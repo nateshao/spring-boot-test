ee81266e1a4482224f4541d68b1858a51d94f9a0
nateshao <shaotongjie@gmail.com>
2020-07-30 00:19:34 +0800

    add AOP

---
+++ nateshao-boot-06-log-aop/src/main/java/com/nateshao/config/LogAop.java
@@ -0,0 +1,12 @@
+package com.nateshao.config;
+
+/**
+ * @date Created by 邵桐杰 on 2020/7/30 0:18
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ */
+public class LogAop {
+
+}
