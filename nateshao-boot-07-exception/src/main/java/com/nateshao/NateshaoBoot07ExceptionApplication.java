d7613c3dd591cdfa5b990d4588a13c1f31c21778
nateshao <shaotongjie@gmail.com>
2020-07-30 23:47:34 +0800

    Springboot 异常处理

---
+++ nateshao-boot-07-exception/src/main/java/com/nateshao/NateshaoBoot07ExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot07ExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot07ExceptionApplication.class, args);
+    }
+
+}
