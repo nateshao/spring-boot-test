5f7562350f723a2c9a74c08590e8400ba6fad174
nateshao <shaotongjie@gmail.com>
2020-08-01 22:00:15 +0800

    初始化完成

---
+++ nateshao-boot-08-freemarker/src/main/java/com/nateshao/NateshaoBoot08FreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot08FreemarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot08FreemarkerApplication.class, args);
+    }
+
+}
