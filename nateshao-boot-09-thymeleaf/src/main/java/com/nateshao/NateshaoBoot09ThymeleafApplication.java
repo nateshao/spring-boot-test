28d5f9dd8faef0cf4257765e56a8256516252b36
nateshao <shaotongjie@gmail.com>
2020-08-01 23:02:59 +0800

    初始化完成

---
+++ nateshao-boot-09-thymeleaf/src/main/java/com/nateshao/NateshaoBoot09ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot09ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot09ThymeleafApplication.class, args);
+    }
+
+}
