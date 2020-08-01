79e202bbe0723d33a38a70f473fb79425ed74b2a
nateshao <shaotongjie@gmail.com>
2020-08-02 00:08:36 +0800

    初始化完成

---
+++ nateshao-boot-09-thymeleaf/src/main/java/com/nateshao/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.nateshao.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @date Created by 邵桐杰 on 2020/8/1 23:59
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@Controller
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+    @PostMapping("/login")
+    public ModelAndView login(User user, HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject(user);
+        mv.setViewName("redirect:/");
+
+        request.getSession().setAttribute("user", user);
+        return mv;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("page/login");
+    }
+}
