76ef818eca0ac364d8893d412db6cabc1f9b2849
nateshao <shaotongjie@gmail.com>
2020-08-02 12:27:03 +0800

    初始化完成

---
+++ nateshao-boot-11-enjoy/src/main/java/com/nateshao/nateshaoboot11enjoy/NateshaoBoot11EnjoyApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot11enjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot11EnjoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot11EnjoyApplication.class, args);
+    }
+
+}
---
+++ nateshao-boot-11-enjoy/src/test/java/com/nateshao/nateshaoboot11enjoy/NateshaoBoot11EnjoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot11enjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBoot11EnjoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
