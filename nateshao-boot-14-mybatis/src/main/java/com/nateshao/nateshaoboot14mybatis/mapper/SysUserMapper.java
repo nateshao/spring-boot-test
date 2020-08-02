6e091fd28b25f3e6d72f30563eafaf23082f3796
nateshao <shaotongjie@gmail.com>
2020-08-02 22:05:21 +0800

    测试连接数据库完成

---
+++ nateshao-boot-14-mybatis/src/main/java/com/nateshao/nateshaoboot14mybatis/mapper/SysUserMapper.java
@@ -0,0 +1,20 @@
+package com.nateshao.nateshaoboot14mybatis.mapper;
+
+import com.nateshao.nateshaoboot14mybatis.domain.SysUser;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @date Created by 邵桐杰 on 2020/8/2 21:42
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@Mapper
+public interface SysUserMapper {
+    List<SysUser> selectAll();
+}
--- nateshao-boot-14-mybatis/src/test/java/com/nateshao/nateshaoboot14mybatis/NateshaoBoot14MybatisApplicationTests.java
+++ nateshao-boot-14-mybatis/src/test/java/com/nateshao/nateshaoboot14mybatis/NateshaoBoot14MybatisApplicationTests.java
@@ -1,13 +1,19 @@
 package com.nateshao.nateshaoboot14mybatis;
 
+import com.nateshao.nateshaoboot14mybatis.mapper.SysUserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NateshaoBoot14MybatisApplicationTests {
 
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
     @Test
     void contextLoads() {
+        System.out.println(sysUserMapper.selectAll());
     }
 
 }
