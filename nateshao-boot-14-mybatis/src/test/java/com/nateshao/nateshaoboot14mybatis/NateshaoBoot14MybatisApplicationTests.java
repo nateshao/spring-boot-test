515fc70be3a37cbfaf05093dba3a138941b259a0
nateshao <shaotongjie@gmail.com>
2020-08-02 21:38:43 +0800

    nateshao-boot-14-mybatis初始化完成

---
+++ nateshao-boot-14-mybatis/src/test/java/com/nateshao/nateshaoboot14mybatis/NateshaoBoot14MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot14mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBoot14MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
