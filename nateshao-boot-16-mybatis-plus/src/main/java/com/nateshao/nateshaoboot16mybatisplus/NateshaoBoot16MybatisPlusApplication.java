f8389e160afab66deb849280d2e6527fd4ab0e77
nateshao <shaotongjie@gmail.com>
2020-08-03 22:44:00 +0800

    nateshao-boot-16-mybatis-plus初始化完成

---
+++ nateshao-boot-16-mybatis-plus/src/main/java/com/nateshao/nateshaoboot16mybatisplus/NateshaoBoot16MybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot16mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot16MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot16MybatisPlusApplication.class, args);
+    }
+
+}
---
+++ nateshao-boot-16-mybatis-plus/src/test/java/com/nateshao/nateshaoboot16mybatisplus/NateshaoBoot16MybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot16mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBoot16MybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
