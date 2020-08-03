6c4ffdfdd997daa2b950804b8d1a097109736386
nateshao <shaotongjie@gmail.com>
2020-08-03 23:54:32 +0800

    nateshao-boot-17-beetlsql初始化完成

---
+++ nateshao-boot-17-beetlsql/src/main/java/com/nateshao/nateshaoboot17beetlsql/NateshaoBoot17BeetlsqlApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot17beetlsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot17BeetlsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot17BeetlsqlApplication.class, args);
+    }
+
+}
---
+++ nateshao-boot-17-beetlsql/src/test/java/com/nateshao/nateshaoboot17beetlsql/NateshaoBoot17BeetlsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot17beetlsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBoot17BeetlsqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
