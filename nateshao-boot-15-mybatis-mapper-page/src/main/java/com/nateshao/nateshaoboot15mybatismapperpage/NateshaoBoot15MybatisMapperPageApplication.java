c95e3552a606227f58829a7cc8d51c91d4fa5100
nateshao <shaotongjie@gmail.com>
2020-08-03 21:20:44 +0800

    nateshao-boot-15-mybatis-mapper-page 初始化完成

---
+++ nateshao-boot-15-mybatis-mapper-page/src/main/java/com/nateshao/nateshaoboot15mybatismapperpage/NateshaoBoot15MybatisMapperPageApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot15mybatismapperpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot15MybatisMapperPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot15MybatisMapperPageApplication.class, args);
+    }
+
+}
---
+++ nateshao-boot-15-mybatis-mapper-page/src/test/java/com/nateshao/nateshaoboot15mybatismapperpage/NateshaoBoot15MybatisMapperPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaoboot15mybatismapperpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBoot15MybatisMapperPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
