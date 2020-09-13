7928db491a6c978e3a40efcf9c0a82fe9267d0fd
nateshao <shaotongjie@gmail.com>
2020-09-13 23:23:06 +0800

    add nateshao-boot-00-text-all

--- nateshao-boot-00-text-all/nateshao-boot-chapter02/src/main/java/com/nateshao/domain/Student.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter02/src/main/java/com/nateshao/domain/Student.java
@@ -5,14 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-/**
- * @date Created by 邵桐杰 on 2020/9/13 23:20
- * @微信公众号 千羽的编程时光
- * @个人网站 www.nateshao.cn
- * @博客 https://nateshao.gitee.io
- * @GitHub https://github.com/nateshao
- * @Gitee https://gitee.com/nateshao
- */
+
 @Component  // 用于将Student类作为Bean注入到Spring容器中
 public class Student {
     @Value("${person.id}")
