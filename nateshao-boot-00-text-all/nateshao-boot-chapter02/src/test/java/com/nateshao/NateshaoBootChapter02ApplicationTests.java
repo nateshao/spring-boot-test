654e728f44c3739083688e047531db944497e13f
nateshao <shaotongjie@gmail.com>
2020-09-13 23:27:27 +0800

    add nateshao-boot-00-text-all

--- nateshao-boot-00-text-all/nateshao-boot-chapter02/src/test/java/com/nateshao/NateshaoBootChapter02ApplicationTests.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter02/src/test/java/com/nateshao/NateshaoBootChapter02ApplicationTests.java
@@ -1,13 +1,52 @@
 package com.nateshao;
 
+import com.nateshao.domain.MyProperties;
+import com.nateshao.domain.Person;
+import com.nateshao.domain.Student;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class NateshaoBootChapter02ApplicationTests {
+public class NateshaoBootChapter02ApplicationTests {
+    @Autowired
+    private Person person;
+    @Autowired
+    private Student student;
+    @Autowired
+    private MyProperties myProperties;
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println(person);
+    }
+
+    @Test
+    public void studentTest() {
+        System.out.println(student);
+    }
+
+    @Test
+    public void myPropertiesTest() {
+        System.out.println(myProperties);
+    }
+
+    @Test
+    public void iocTest() {
+        System.out.println(applicationContext.containsBean("myService"));
+    }
+    @Value("${tom.description}")
+    private String description;
+    @Test
+    public void placeholderTest() {
+        System.out.println(description);
     }
 
 }
