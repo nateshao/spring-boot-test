dcc97d32403f279a2c62373abfe2a71b32fa2001
nateshao <shaotongjie@gmail.com>
2020-09-13 23:05:40 +0800

    add nateshao-boot-chapter02

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter02/src/main/java/com/nateshao/NateshaoBootChapter02Application.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBootChapter02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBootChapter02Application.class, args);
+    }
+
+}
