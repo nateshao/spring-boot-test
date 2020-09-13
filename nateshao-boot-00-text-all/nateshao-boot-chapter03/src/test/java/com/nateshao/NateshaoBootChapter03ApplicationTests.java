f9c5ed08fbd3262851b7e9bf1b587c9bd71f1142
nateshao <shaotongjie@gmail.com>
2020-09-13 23:14:53 +0800

    add nateshao-boot-chapter01

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter03/src/test/java/com/nateshao/NateshaoBootChapter03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBootChapter03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
