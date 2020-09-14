f006f926ae92aa746b5d21a8abe91ed597627bd8
nateshao <shaotongjie@gmail.com>
2020-09-14 20:47:03 +0800

    add config

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter03-jpa/src/main/java/com/nateshao/domain/Article.java
@@ -0,0 +1,60 @@
+package com.nateshao.domain;
+
+import java.util.List;
+
+/**
+ * @date Created by 邵桐杰 on 2020/9/14 20:46
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+public class Article {
+    private Integer id;
+    private String title;
+    private String content;
+    private List<Comment> commentList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", commentList=" + commentList +
+                '}';
+    }
+}
