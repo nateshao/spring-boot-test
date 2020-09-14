27661925b3a18f25c38da41e1ee1ccadf5abbf87
nateshao <shaotongjie@gmail.com>
2020-09-14 20:45:48 +0800

    add config

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter03-jpa/src/main/java/com/nateshao/domain/Comment.java
@@ -0,0 +1,59 @@
+package com.nateshao.domain;
+
+/**
+ * @date Created by 邵桐杰 on 2020/9/14 20:42
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+
+public class Comment {
+    private Integer id;
+    private String content;
+    private String author;
+    private Integer aId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getaId() {
+        return aId;
+    }
+
+    public void setaId(Integer aId) {
+        this.aId = aId;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                ", aId=" + aId +
+                '}';
+    }
+}
