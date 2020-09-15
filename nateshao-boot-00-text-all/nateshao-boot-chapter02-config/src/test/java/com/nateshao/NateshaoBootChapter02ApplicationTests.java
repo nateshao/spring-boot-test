53d4d45f97a3b2a6b568a1ad1ae0da034ce53a29
nateshao <shaotongjie@gmail.com>
2020-09-15 11:26:12 +0800

    add 配置文件

--- nateshao-boot-00-text-all/nateshao-boot-chapter02-config/src/test/java/com/nateshao/NateshaoBootChapter02ApplicationTests.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter02-config/src/test/java/com/nateshao/NateshaoBootChapter02ApplicationTests.java
@@ -3,7 +3,7 @@ package com.nateshao;
 import com.nateshao.domain.MyProperties;
 import com.nateshao.domain.Person;
 import com.nateshao.domain.Student;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
