faa01473314cd7b9e3898fb7bfc69a84a47ffe8d
nateshao <shaotongjie@gmail.com>
2020-10-19 15:54:38 +0800

    编写代码

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter04-thymeleaf/src/main/java/com/nateshao/config/MyLocalResovel.java
@@ -0,0 +1,30 @@
+package com.nateshao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * @date Created by 邵桐杰 on 2020/10/19 15:53
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@Configuration
+public class MyLocalResovel implements LocaleResolver {
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+        return null;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
