aa236a97be4f230f0d70b2f7acd86392c065dd1a
nateshao <shaotongjie@gmail.com>
2020-10-19 20:03:56 +0800

    中英文转换完成

--- nateshao-boot-00-text-all/nateshao-boot-chapter04-thymeleaf/src/main/java/com/nateshao/config/MyLocalResovel.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter04-thymeleaf/src/main/java/com/nateshao/config/MyLocalResovel.java
@@ -2,6 +2,7 @@ package com.nateshao.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.LocaleResolver;
 
@@ -19,50 +20,34 @@ import java.util.Locale;
  */
 @Configuration
 public class MyLocalResovel implements LocaleResolver {
-
+    // 自定义区域解析方式
     @Override
     public Locale resolveLocale(HttpServletRequest httpServletRequest) {
-        String l = httpServletRequest.getParameter("1");
-        String header = httpServletRequest.getHeader("Accept-Language");
 
-        Locale locale = null;
-        if (!StringUtils.isEmpty(1)){
+        String l = httpServletRequest.getParameter("l");
+        String header = httpServletRequest.getHeader("Accept-Language");
+        Locale locale=null;
+
+        if(!StringUtils.isEmpty(l)){
             String[] split = l.split("_");
-            locale = new Locale(split[0],split[1]);
+            locale=new Locale(split[0],split[1]);
         }else {
+            // Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7
             String[] splits = header.split(",");
             String[] split = splits[0].split("-");
-            locale = new Locale(split[0],split[1]);
+            locale=new Locale(split[0],split[1]);
         }
-
         return locale;
     }
-
     @Override
-    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
-
+    public void setLocale(HttpServletRequest httpServletRequest, @Nullable
+            HttpServletResponse httpServletResponse, @Nullable Locale locale) {
     }
 
     @Bean
     public LocaleResolver localeResolver(){
         return new MyLocalResovel();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
