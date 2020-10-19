801edd4fff010cd18d2904ad0a9d7a7bb3558b76
nateshao <shaotongjie@gmail.com>
2020-10-19 15:45:23 +0800

    编写代码

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter04-thymeleaf/src/main/java/com/nateshao/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.nateshao.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @date Created by 邵桐杰 on 2020/10/19 15:44
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@Controller
+public class LoginController {
+
+
+}
