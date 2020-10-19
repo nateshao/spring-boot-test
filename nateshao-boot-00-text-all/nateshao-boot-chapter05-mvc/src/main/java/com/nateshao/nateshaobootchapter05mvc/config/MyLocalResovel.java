f5c360df83b6fd2b2a07fb54ddc81a020e12e288
nateshao <shaotongjie@gmail.com>
2020-10-19 20:17:14 +0800

    初始化nateshao-boot-chapter05-mvc

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/config/MyLocalResovel.java
@@ -0,0 +1,12 @@
+package com.nateshao.nateshaobootchapter05mvc.config;
+
+/**
+ * @date Created by 邵桐杰 on 2020/10/19 20:16
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+public class MyLocalResovel {
+}
