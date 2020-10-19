c44a1937b1cb696c0640f867e5e4bd473c282389
nateshao <shaotongjie@gmail.com>
2020-10-19 20:08:53 +0800

    初始化nateshao-boot-chapter05-mvc

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/test/java/com/nateshao/nateshaobootchapter05mvc/NateshaoBootChapter05MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.nateshaobootchapter05mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBootChapter05MvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
