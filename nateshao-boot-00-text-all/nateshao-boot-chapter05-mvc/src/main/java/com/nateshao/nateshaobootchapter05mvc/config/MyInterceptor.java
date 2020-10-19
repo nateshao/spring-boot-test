84cc0885b10fbe0f39facba789ee684cfeae1987
nateshao <shaotongjie@gmail.com>
2020-10-19 20:50:20 +0800

    拦截类

--- nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/config/MyInterceptor.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/config/MyInterceptor.java
@@ -1,5 +1,12 @@
 package com.nateshao.nateshaobootchapter05mvc.config;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @date Created by 邵桐杰 on 2020/10/19 20:16
  * @微信公众号 千羽的编程时光
@@ -8,5 +15,29 @@ package com.nateshao.nateshaobootchapter05mvc.config;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
-public class MyInterceptor {
+
+/**
+ * 拦截类
+ */
+
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        Object loginUser = request.getSession().getAttribute("loginUser");
+
+
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
 }
