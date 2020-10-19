886bf897964e39257cbb8e403b47b56e5f94429f
nateshao <shaotongjie@gmail.com>
2020-10-19 15:48:32 +0800

    编写代码

--- nateshao-boot-00-text-all/nateshao-boot-chapter04-thymeleaf/src/main/java/com/nateshao/controller/LoginController.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter04-thymeleaf/src/main/java/com/nateshao/controller/LoginController.java
@@ -1,6 +1,10 @@
 package com.nateshao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
 
 /**
  * @date Created by 邵桐杰 on 2020/10/19 15:44
@@ -13,5 +17,35 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class LoginController {
 
+    @GetMapping("/toLoginPage")
+    public String toLoginPage(Model model){
+        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+        return "login";
+
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
