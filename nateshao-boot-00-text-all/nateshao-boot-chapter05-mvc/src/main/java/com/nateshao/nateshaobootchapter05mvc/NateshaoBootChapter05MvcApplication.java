d671abf47df5aac07f4ac06d6497a528c918b683
nateshao <shaotongjie@gmail.com>
2020-10-19 23:26:43 +0800

    文件上传与下载

--- nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/NateshaoBootChapter05MvcApplication.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/NateshaoBootChapter05MvcApplication.java
@@ -2,11 +2,17 @@ package com.nateshao.nateshaobootchapter05mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class NateshaoBootChapter05MvcApplication {
+public class NateshaoBootChapter05MvcApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NateshaoBootChapter05MvcApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NateshaoBootChapter05MvcApplication.class, args);
