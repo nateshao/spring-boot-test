fe792e39a0e9bb9264e2c05bc8bc3e69c5a7b791
nateshao <shaotongjie@gmail.com>
2020-10-19 21:55:37 +0800

    listener

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/servletComponent/MyListener.java
@@ -0,0 +1,29 @@
+package com.nateshao.nateshaobootchapter05mvc.servletComponent;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @date Created by 邵桐杰 on 2020/10/19 21:53
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@Component
+public class MyListener implements ServletContextListener {
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("context initialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("context destoryed..");
+    }
+}
