78835245660be00e1b6f0cc77d1bbddeba01042b
nateshao <shaotongjie@gmail.com>
2020-10-19 20:52:38 +0800

    拦截类

--- nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/config/MyInterceptor.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter05-mvc/src/main/java/com/nateshao/nateshaobootchapter05mvc/config/MyInterceptor.java
@@ -27,11 +27,11 @@ public class MyInterceptor implements HandlerInterceptor {
         String uri = request.getRequestURI();
         Object loginUser = request.getSession().getAttribute("loginUser");
 
-        if (uri.startsWith("/admin")&&loginUser ==null){
-
+        if (uri.startsWith("/admin")&&loginUser == null){
+            response.sendRedirect("/toLoginPage");
+            return false;
         }
-
-        return false;
+        return true;
     }
 
     @Override
