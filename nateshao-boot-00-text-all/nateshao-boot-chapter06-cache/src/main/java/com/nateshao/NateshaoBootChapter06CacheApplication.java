ced3f817ad43b5e2807cd80cc4a9623b899000a8
nateshao <shaotongjie@gmail.com>
2020-10-27 10:56:36 +0800

    nateshao-boot-chapter06-cache

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter06-cache/src/main/java/com/nateshao/NateshaoBootChapter06CacheApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBootChapter06CacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBootChapter06CacheApplication.class, args);
+    }
+
+}
