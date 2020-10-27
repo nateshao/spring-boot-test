66cc98874f18ba91251de660740bb5ff05a70e55
nateshao <shaotongjie@gmail.com>
2020-10-27 11:02:42 +0800

    nateshao-boot-chapter06-cache

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter06-cache/src/test/java/com/nateshao/NateshaoBootChapter06CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBootChapter06CacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
