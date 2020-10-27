728482bb94ac5336d78714ab08cb68c468b0a52f
nateshao <shaotongjie@gmail.com>
2020-10-27 21:50:42 +0800

    nateshao-boot-chapter07-security

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/NateshaoBootChapter07SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBootChapter07SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBootChapter07SecurityApplication.class, args);
+    }
+
+}
