6c4075ca8fd204aca30375a47fac8502862d04fe
nateshao <shaotongjie@gmail.com>
2020-10-27 11:09:12 +0800

    添加sql

--- nateshao-boot-00-text-all/nateshao-boot-chapter06-cache/src/main/java/com/nateshao/domain/Comment.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter06-cache/src/main/java/com/nateshao/domain/Comment.java
@@ -1,5 +1,10 @@
 package com.nateshao.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @date Created by 邵桐杰 on 2020/10/27 11:06
  * @微信公众号 千羽的编程时光
@@ -8,5 +13,23 @@ package com.nateshao.domain;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
+@Entity(name = "t_comment")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String content;
+    private String author;
+    private Integer aId;
+
 }
+
+
+
+
+
+
+
+
+
+
