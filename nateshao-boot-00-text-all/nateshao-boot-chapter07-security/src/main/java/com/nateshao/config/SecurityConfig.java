d54a77d8a38d1929abab4bf0646ba3b4e008527a
nateshao <shaotongjie@gmail.com>
2020-10-28 22:21:05 +0800

    JDBC用户认证

--- nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/config/SecurityConfig.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.nateshao.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * @date Created by 邵桐杰 on 2020/10/28 22:00
@@ -16,17 +19,30 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /******************* 自定义用户认证 ***********************/
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//        auth.inMemoryAuthentication().passwordEncoder(encoder)
+//                .withUser("shitou").password(encoder.encode("123456")).roles("common")
+//                .and()
+//                .withUser("李四").password(encoder.encode("123456")).roles("vip");
+//
+//
+////        super.configure(auth);
+//    }
+
+    /******************* JDBC用户认证 ***********************/
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-        auth.inMemoryAuthentication().passwordEncoder(encoder)
-                .withUser("shitou").password(encoder.encode("123456")).roles("common")
-                .and()
-                .withUser("李四").password(encoder.encode("123456")).roles("vip");
 
 
-//        super.configure(auth);
     }
 }
 
