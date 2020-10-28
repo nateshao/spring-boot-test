47c591e95c2e2117cbc48a91d0699c2b9fc11890
nateshao <shaotongjie@gmail.com>
2020-10-28 22:06:23 +0800

    SecurityConfig

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.nateshao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @date Created by 邵桐杰 on 2020/10/28 22:00
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        auth.inMemoryAuthentication().passwordEncoder(encoder)
+                .withUser("shitou").password(encoder.encode("123456")).roles("common")
+                .and()
+                .withUser("李四").password(encoder.encode("123456")).roles("vip");
+
+
+        super.configure(auth);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
