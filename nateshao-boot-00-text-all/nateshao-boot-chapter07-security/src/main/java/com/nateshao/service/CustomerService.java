1e6d5c1397dc6ada7ebf6b16689428d46ca28689
nateshao <shaotongjie@gmail.com>
2020-11-02 22:17:47 +0800

    UserDetailsService进行身份认证

--- nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/service/CustomerService.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/service/CustomerService.java
@@ -28,29 +28,30 @@ public class CustomerService {
     private RedisTemplate redisTemplate;
 
     // 业务控制：使用唯一用户名查询用户信息
-    public Customer getCustomer(String username){
-        Customer customer=null;
-        Object o = redisTemplate.opsForValue().get("customer_"+username);
-        if(o!=null){
-            customer=(Customer)o;
-        }else {
+    public Customer getCustomer(String username) {
+        Customer customer = null;
+        Object o = redisTemplate.opsForValue().get("customer_" + username);
+        if (o != null) {
+            customer = (Customer) o;
+        } else {
             customer = customerRepository.findByUsername(username);
-            if(customer!=null){
-                redisTemplate.opsForValue().set("customer_"+username,customer);
+            if (customer != null) {
+                redisTemplate.opsForValue().set("customer_" + username, customer);
             }
         }
         return customer;
     }
+
     // 业务控制：使用唯一用户名查询用户权限
-    public List<Authority> getCustomerAuthority(String username){
-        List<Authority> authorities=null;
-        Object o = redisTemplate.opsForValue().get("authorities_"+username);
-        if(o!=null){
-            authorities=(List<Authority>)o;
-        }else {
-            authorities=authorityRepository.findAuthoritiesByUsername(username);
-            if(authorities.size()>0){
-                redisTemplate.opsForValue().set("authorities_"+username,authorities);
+    public List<Authority> getCustomerAuthority(String username) {
+        List<Authority> authorities = null;
+        Object o = redisTemplate.opsForValue().get("authorities_" + username);
+        if (o != null) {
+            authorities = (List<Authority>) o;
+        } else {
+            authorities = authorityRepository.findAuthoritiesByUsername(username);
+            if (authorities.size() > 0) {
+                redisTemplate.opsForValue().set("authorities_" + username, authorities);
             }
         }
         return authorities;
