65ef5b1985e0db3081eb6eb281f014c5bacc5ef4
nateshao <shaotongjie@gmail.com>
2020-11-02 22:35:12 +0800

    UserDetailsService进行身份认证

--- nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/config/SecurityConfig.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter07-security/src/main/java/com/nateshao/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.nateshao.config;
 
 import com.nateshao.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 
 import javax.sql.DataSource;
 
@@ -61,21 +64,71 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     /******************* UserDetailsService进行身份认证 ***********************/
 
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        // 2、使用JDBC进行身份认证
-        String userSQL ="select username,password,valid from t_customer " +
-                "where username = ?";
-        String authoritySQL="select c.username,a.authority from t_customer c,t_authority a,"+
-                "t_customer_authority ca where ca.customer_id=c.id " +
-                "and ca.authority_id=a.id and c.username =?";
-        auth.jdbcAuthentication().passwordEncoder(encoder)
-                .dataSource(dataSource)
-                .usersByUsernameQuery(userSQL)
-                .authoritiesByUsernameQuery(authoritySQL);
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        // 2、使用JDBC进行身份认证
+//        String userSQL ="select username,password,valid from t_customer " +
+//                "where username = ?";
+//        String authoritySQL="select c.username,a.authority from t_customer c,t_authority a,"+
+//                "t_customer_authority ca where ca.customer_id=c.id " +
+//                "and ca.authority_id=a.id and c.username =?";
+//        auth.jdbcAuthentication().passwordEncoder(encoder)
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery(userSQL)
+//                .authoritiesByUsernameQuery(authoritySQL);
+//
+//        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+//    }
 
-        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+
+
+    /******************* 用户授权管理自定义配置 ***********************/
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 自定义用户授权管理
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                // 需要对static文件夹下静态资源进行统一放行
+                .antMatchers("/login/**").permitAll()
+                .antMatchers("/detail/common/**").hasRole("common")
+                .antMatchers("/detail/vip/**").hasRole("vip")
+                .anyRequest().authenticated();
+
+        // 自定义用户登录控制
+        http.formLogin()
+                .loginPage("/userLogin").permitAll()
+                .usernameParameter("name").passwordParameter("pwd")
+                .defaultSuccessUrl("/")
+                .failureUrl("/userLogin?error");
+
+        // 自定义用户退出控制
+        http.logout()
+                .logoutUrl("/mylogout")
+                .logoutSuccessUrl("/");
+
+        // 定制Remember-me记住我功能
+        http.rememberMe()
+                .rememberMeParameter("rememberme")
+                .tokenValiditySeconds(200)
+                // 对cookie信息进行持久化管理
+                .tokenRepository(tokenRepository());
+
+        // 可以关闭Spring Security默认开启的CSRF防护功能
+//        http.csrf().disable();
+
+    }
+    /**
+     * 持久化Token存储
+     * @return
+     */
+    @Bean
+    public JdbcTokenRepositoryImpl tokenRepository(){
+        JdbcTokenRepositoryImpl jr=new JdbcTokenRepositoryImpl();
+        jr.setDataSource(dataSource);
+        return jr;
     }
 
 
