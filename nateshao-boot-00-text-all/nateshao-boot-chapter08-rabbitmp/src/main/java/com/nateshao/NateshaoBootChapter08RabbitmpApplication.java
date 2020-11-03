b6a8e135c8bb51451e4636a8efd9fb94535b2d70
nateshao <shaotongjie@gmail.com>
2020-11-03 20:40:47 +0800

    初始化完成

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter08-rabbitmp/src/main/java/com/nateshao/NateshaoBootChapter08RabbitmpApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBootChapter08RabbitmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBootChapter08RabbitmpApplication.class, args);
+    }
+
+}
