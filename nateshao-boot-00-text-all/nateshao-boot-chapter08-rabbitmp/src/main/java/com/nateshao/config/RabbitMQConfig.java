5874d2fdfabf8be76e31f35b53e256cbe76cecbd
nateshao <shaotongjie@gmail.com>
2020-11-05 21:00:59 +0800

    初始化完成

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter08-rabbitmp/src/main/java/com/nateshao/config/RabbitMQConfig.java
@@ -0,0 +1,14 @@
+package com.nateshao.config;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/5 20:59
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+
+
+public class RabbitMQConfig {
+}
