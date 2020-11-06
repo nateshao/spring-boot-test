706b337b0a1a7285115f9102f890a00582bb0c9d
nateshao <shaotongjie@gmail.com>
2020-11-06 09:36:36 +0800

    MyAsyncController

--- nateshao-boot-00-text-all/nateshao-boot-chapter09-task/src/main/java/com/nateshao/controller/MyAsyncController.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter09-task/src/main/java/com/nateshao/controller/MyAsyncController.java
@@ -4,6 +4,7 @@ import com.nateshao.service.MyAsyncService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @date Created by 邵桐杰 on 2020/11/6 9:26
@@ -19,5 +20,13 @@ public class MyAsyncController {
     @Autowired
     private MyAsyncService myAsyncService;
 
+    @GetMapping("/sendSMS")
+    public String sendSMS() throws Exception {
+        long startTime = System.currentTimeMillis();
+        myAsyncService.sendSMS();
+        long endTime = System.currentTimeMillis();
+        System.out.println("主流程耗时：" + (endTime - startTime));
+        return "success";
+    }
 
 }
