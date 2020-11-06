bac60def85421348a995f27498825900f88acde6
nateshao <shaotongjie@gmail.com>
2020-11-06 10:41:29 +0800

    nateshao-boot-10-blog

---
+++ nateshao-boot-00-text-all/nateshao-boot-10-blog/src/main/java/com/nateshao/NateshaoBoot10BlogApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot10BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot10BlogApplication.class, args);
+    }
+
+}
