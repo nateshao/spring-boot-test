d59c612205b00edc7bd9b0890e695067ed4f2eb0
nateshao <shaotongjie@gmail.com>
2020-11-06 09:28:15 +0800

    MyAsyncController

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter09-task/src/main/java/com/nateshao/service/MyAsyncService.java
@@ -0,0 +1,16 @@
+package com.nateshao.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/6 9:27
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@Service
+public class MyAsyncService {
+
+}
