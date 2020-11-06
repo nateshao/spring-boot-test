69e3de395b0577c131a9a2ab45e626c7bba274fd
nateshao <shaotongjie@gmail.com>
2020-11-06 10:07:03 +0800

    MyAsyncController

--- nateshao-boot-00-text-all/nateshao-boot-chapter09-task/src/test/java/com/nateshao/service/SendEmailServiceTest.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter09-task/src/test/java/com/nateshao/service/SendEmailServiceTest.java
@@ -29,37 +29,37 @@ class SendEmailServiceTest {
 
     @Test
     public void sendSimpleMailTest() {
-        String to="2127269781@qq.com";
-        String subject="【纯文本邮件】标题";
-        String text="Spring Boot纯文本邮件发送内容测试.....";
+        String to = "1210331079@qq.com";
+        String subject = "【纯文本邮件】标题";
+        String text = "Spring Boot纯文本邮件发送内容测试.....";
         // 发送简单邮件
-        sendEmailService.sendSimpleEmail(to,subject,text);
+        sendEmailService.sendSimpleEmail(to, subject, text);
     }
 
     @Test
     public void sendComplexEmailTest() {
-        String to="2127269781@qq.com";
-        String subject="【复杂邮件】标题";
+        String to = "2127269781@qq.com";
+        String subject = "【复杂邮件】标题";
         // 定义邮件内容
         StringBuilder text = new StringBuilder();
         text.append("<html><head></head>");
         text.append("<body><h1>祝大家元旦快乐！</h1>");
         // cid为固定写法，rscId指定一个唯一标识
         String rscId = "img001";
-        text.append("<img src='cid:" +rscId+"'/></body>");
+        text.append("<img src='cid:" + rscId + "'/></body>");
         text.append("</html>");
         // 指定静态资源文件和附件路径
-        String rscPath="F:\\email\\newyear.jpg";
-        String filePath="F:\\email\\元旦放假注意事项.docx";
+        String rscPath = "F:\\email\\newyear.jpg";
+        String filePath = "F:\\email\\元旦放假注意事项.docx";
         // 发送复杂邮件
-        sendEmailService.sendComplexEmail(to,subject,text.toString(),filePath,rscId,rscPath);
+        sendEmailService.sendComplexEmail(to, subject, text.toString(), filePath, rscId, rscPath);
     }
 
 
     @Test
     public void sendTemplateEmailTest() {
-        String to="2127269781@qq.com";
-        String subject="【模板邮件】标题";
+        String to = "2127269781@qq.com";
+        String subject = "【模板邮件】标题";
         // 使用模板邮件定制邮件正文内容
         Context context = new Context();
         context.setVariable("username", "石头");
@@ -67,7 +67,7 @@ class SendEmailServiceTest {
         // 使用TemplateEngine设置要处理的模板页面
         String emailContent = templateEngine.process("emailTemplate_vercode", context);
         // 发送模板邮件
-        sendEmailService.sendTemplateEmail(to,subject,emailContent);
+        sendEmailService.sendTemplateEmail(to, subject, emailContent);
     }
 
 }
