60997bd050e950d006c3ffed85cea92f40ea46e1
nateshao <shaotongjie@gmail.com>
2020-11-06 09:58:05 +0800

    MyAsyncController

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter09-task/src/main/java/com/nateshao/service/ScheduledTaskService.java
@@ -0,0 +1,40 @@
+package com.nateshao.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/6 9:57
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+@Service
+public class ScheduledTaskService {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private Integer count1 = 1;
+    private Integer count2 = 1;
+    private Integer count3 = 1;
+
+    @Scheduled(fixedRate = 60000)
+    public void scheduledTaskImmediately() {
+        System.out.println(String.format("fixedRate第%s次执行，当前时间为：%s", count1++, dateFormat.format(new Date())));
+    }
+
+    @Scheduled(fixedDelay = 60000)
+    public void scheduledTaskAfterSleep() throws InterruptedException {
+        System.out.println(String.format("fixedDelay第%s次执行，当前时间为：%s", count2++, dateFormat.format(new Date())));
+        Thread.sleep(10000);
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    public void scheduledTaskCron(){
+        System.out.println(String.format("cron第%s次执行，当前时间为：%s",count3++, dateFormat.format(new Date())));
+    }
+
+}
+
