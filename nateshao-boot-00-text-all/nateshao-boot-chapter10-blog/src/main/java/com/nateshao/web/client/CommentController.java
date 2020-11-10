3b71760f36b9f47edae0de6afdd338e6825689ed
nateshao <shaotongjie@gmail.com>
2020-11-10 10:29:52 +0800

    nateshao-boot-10-blog

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/web/client/CommentController.java
@@ -0,0 +1,12 @@
+package com.nateshao.web.client;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/10 10:29
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+public class CommentController {
+}
