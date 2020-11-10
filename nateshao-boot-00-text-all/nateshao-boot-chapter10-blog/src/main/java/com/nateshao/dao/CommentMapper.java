0ef69d0bb491aed3fd1baa98d03dd326053505a5
nateshao <shaotongjie@gmail.com>
2020-11-10 10:43:42 +0800

    CommentMapper

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/dao/CommentMapper.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/dao/CommentMapper.java
@@ -1,5 +1,13 @@
 package com.nateshao.dao;
 
+import com.nateshao.model.domain.Comment;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/10 10:42
  * @微信公众号 千羽的编程时光
@@ -8,5 +16,26 @@ package com.nateshao.dao;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
+@Mapper
 public interface CommentMapper {
+    // 分页展示某个文章的评论
+    @Select("SELECT * FROM t_comment WHERE article_id=#{aid} ORDER BY id DESC")
+    public List<Comment> selectCommentWithPage(Integer aid);
+
+    // 后台查询最新几条评论
+    @Select("SELECT * FROM t_comment ORDER BY id DESC")
+    public List<Comment> selectNewComment();
+
+    // 发表评论
+    @Insert("INSERT INTO t_comment (article_id,created,author,ip,content)" +
+            " VALUES (#{articleId}, #{created},#{author},#{ip},#{content})")
+    public void pushComment(Comment comment);
+
+    // 站点服务统计，统计评论数量
+    @Select("SELECT COUNT(1) FROM t_comment")
+    public Integer countComment();
+
+    // 通过文章id删除评论信息
+    @Delete("DELETE FROM t_comment WHERE article_id=#{aid}")
+    public void deleteCommentWithId(Integer aid);
 }
