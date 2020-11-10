0e8794081d587c4f7be653f91382d7536ed8cbba
nateshao <shaotongjie@gmail.com>
2020-11-10 10:00:26 +0800

    nateshao-boot-10-blog

---
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/model/ResponseData/Statistic.java
@@ -0,0 +1,12 @@
+package com.nateshao.model.ResponseData;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/10 10:00
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+public class Statistic {
+}
