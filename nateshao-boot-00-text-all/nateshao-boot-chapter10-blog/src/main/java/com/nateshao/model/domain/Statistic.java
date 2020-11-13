50aea83bed9c219410c69519d5b93eaddbaeed57
nateshao <shaotongjie@gmail.com>
2020-11-13 15:22:17 +0800

    Statistic

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/model/domain/Statistic.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/model/domain/Statistic.java
@@ -17,4 +17,45 @@ public class Statistic {
     private Integer hits;        // 点击量
     private Integer commentsNum;// 评论总量
 
+    @Override
+    public String toString() {
+        return "Statistic{" +
+                "id=" + id +
+                ", articleId=" + articleId +
+                ", hits=" + hits +
+                ", commentsNum=" + commentsNum +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+    public Integer getHits() {
+        return hits;
+    }
+
+    public void setHits(Integer hits) {
+        this.hits = hits;
+    }
+
+    public Integer getCommentsNum() {
+        return commentsNum;
+    }
+
+    public void setCommentsNum(Integer commentsNum) {
+        this.commentsNum = commentsNum;
+    }
 }
