ba5ea0bb174e620799b3d37f3b1368cf09e072f7
nateshao <shaotongjie@gmail.com>
2020-11-13 15:16:31 +0800

    Article

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/model/domain/Article.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/model/domain/Article.java
@@ -1,5 +1,7 @@
 package com.nateshao.model.domain;
 
+import java.util.Date;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/10 9:56
  * @微信公众号 千羽的编程时光
@@ -9,4 +11,124 @@ package com.nateshao.model.domain;
  * @Gitee https://gitee.com/nateshao
  */
 public class Article {
+
+    private Integer id;          // 文章id
+    private String title;       // 文章标题
+    private String content;     // 文章内容
+    private Date created;       // 文章创建时间
+
+    private Date modified;      // 文章创建时间
+    private String categories; // 文章分类
+    private String tags;        // 文章标签
+    private Boolean allowComment; // 是否允许评论，默认为true
+    private String thumbnail;     // 文章缩略图
+
+    private Integer hits;       // 点击量
+    private Integer commentsNum;  // 评论总量
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", created=" + created +
+                ", modified=" + modified +
+                ", categories='" + categories + '\'' +
+                ", tags='" + tags + '\'' +
+                ", allowComment=" + allowComment +
+                ", thumbnail='" + thumbnail + '\'' +
+                ", hits=" + hits +
+                ", commentsNum=" + commentsNum +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public Boolean getAllowComment() {
+        return allowComment;
+    }
+
+    public void setAllowComment(Boolean allowComment) {
+        this.allowComment = allowComment;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public Integer getHits() {
+        return hits;
+    }
+
+    public void setHits(Integer hits) {
+        this.hits = hits;
+    }
+
+    public Integer getCommentsNum() {
+        return commentsNum;
+    }
+
+    public void setCommentsNum(Integer commentsNum) {
+        this.commentsNum = commentsNum;
+    }
+
 }
