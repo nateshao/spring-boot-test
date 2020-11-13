0c28f19713d3984660a29491cba4fe40ea4a3d3b
nateshao <shaotongjie@gmail.com>
2020-11-13 15:46:53 +0800

    Statistic

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/model/ResponseData/ArticleResponseData.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/model/ResponseData/ArticleResponseData.java
@@ -8,5 +8,16 @@ package com.nateshao.model.ResponseData;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
-public class ArticleResponseData {
+
+/**
+ * 前端请求响应的封装类
+ *
+ */
+public class ArticleResponseData<T> {
+    private T payload;        //服务器响应数据
+    private boolean success; //请求是否成功
+    private String msg;       // 错误信息
+    private int code = -1;   // 状态码
+    private long timestamp; //服务器响应时间
+
 }
