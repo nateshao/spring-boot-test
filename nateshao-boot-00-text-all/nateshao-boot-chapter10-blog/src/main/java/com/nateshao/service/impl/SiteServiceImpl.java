d793c891be0d60b852e8ce90934d12cd9571b734
nateshao <shaotongjie@gmail.com>
2020-11-16 08:47:53 +0800

    ArticleResponseData

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/service/impl/SiteServiceImpl.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/service/impl/SiteServiceImpl.java
@@ -1,5 +1,20 @@
 package com.nateshao.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.nateshao.dao.ArticleMapper;
+import com.nateshao.dao.CommentMapper;
+import com.nateshao.dao.StatisticMapper;
+import com.nateshao.model.ResponseData.StaticticsBo;
+import com.nateshao.model.domain.Article;
+import com.nateshao.model.domain.Comment;
+import com.nateshao.model.domain.Statistic;
+import com.nateshao.service.ISiteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/16 8:47
  * @微信公众号 千羽的编程时光
@@ -8,5 +23,54 @@ package com.nateshao.service.impl;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
-public class SiteServiceImpl {
+
+
+@Service
+@Transactional
+public class SiteServiceImpl implements ISiteService {
+    @Autowired
+    private CommentMapper commentMapper;
+    @Autowired
+    private ArticleMapper articleMapper;
+    @Autowired
+    private StatisticMapper statisticMapper;
+
+    @Override
+    public void updateStatistics(Article article) {
+        Statistic statistic = statisticMapper.selectStatisticWithArticleId(article.getId());
+        statistic.setHits(statistic.getHits()+1);
+        statisticMapper.updateArticleHitsWithId(statistic);
+    }
+
+    @Override
+    public List<Comment> recentComments(int limit) {
+        PageHelper.startPage(1, limit>10 || limit<1 ? 10:limit);
+        List<Comment> byPage = commentMapper.selectNewComment();
+        return byPage;
+    }
+
+    @Override
+    public List<Article> recentArticles(int limit) {
+        PageHelper.startPage(1, limit>10 || limit<1 ? 10:limit);
+        List<Article> list = articleMapper.selectArticleWithPage();
+        // 封装文章统计数据
+        for (int i = 0; i < list.size(); i++) {
+            Article article = list.get(i);
+            Statistic statistic = statisticMapper.selectStatisticWithArticleId(article.getId());
+            article.setHits(statistic.getHits());
+            article.setCommentsNum(statistic.getCommentsNum());
+        }
+        return list;
+    }
+
+    @Override
+    public StaticticsBo getStatistics() {
+        StaticticsBo staticticsBo = new StaticticsBo();
+        Integer articles = articleMapper.countArticle();
+        Integer comments = commentMapper.countComment();
+        staticticsBo.setArticles(articles);
+        staticticsBo.setComments(comments);
+        return staticticsBo;
+    }
 }
+
