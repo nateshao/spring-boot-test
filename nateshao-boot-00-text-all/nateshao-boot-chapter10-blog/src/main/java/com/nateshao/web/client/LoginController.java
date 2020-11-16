ef609c9477cd9f790f476ee3c1acd13811359bfa
nateshao <shaotongjie@gmail.com>
2020-11-16 08:55:21 +0800

    ArticleResponseData

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/web/client/LoginController.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/web/client/LoginController.java
@@ -1,5 +1,12 @@
 package com.nateshao.web.client;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/10 10:30
  * @微信公众号 千羽的编程时光
@@ -8,5 +15,35 @@ package com.nateshao.web.client;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
+@Controller
 public class LoginController {
+    // 向登录页面跳转，同时封装原始页面地址
+    @GetMapping(value = "/login")
+    public String login(HttpServletRequest request, Map map) {
+        // 分别获取请求头和参数url中的原始访问路径
+        String referer = request.getHeader("Referer");
+        String url = request.getParameter("url");
+        System.out.println("referer= "+referer);
+        System.out.println("url= "+url);
+
+        // 如果参数url中已经封装了原始页面路径，直接返回该路径
+        if (url!=null && !url.equals("")){
+            map.put("url",url);
+            // 如果请求头本身包含登录，将重定向url设为空，让后台通过用户角色进行选择跳转
+        }else if (referer!=null && referer.contains("/login")){
+            map.put("url", "");
+        }else {
+            // 否则的话，就记住请求头中的原始访问路径
+            map.put("url", referer);
+        }
+        return "comm/login";
+    }
+
+    // 对Security拦截的无权限访问异常处理路径映射
+    @GetMapping(value = "/errorPage/{page}/{code}")
+    public String AccessExecptionHandler(@PathVariable("page") String page, @PathVariable("code") String code) {
+        return page+"/"+code;
+    }
 }
+
+
