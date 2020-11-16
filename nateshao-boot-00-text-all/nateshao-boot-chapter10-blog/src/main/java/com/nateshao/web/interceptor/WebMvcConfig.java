baa484e65406b538b42ea5411e65335c7bb4a5be
nateshao <shaotongjie@gmail.com>
2020-11-16 08:56:29 +0800

    ArticleResponseData

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/web/interceptor/WebMvcConfig.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/web/interceptor/WebMvcConfig.java
@@ -1,5 +1,10 @@
 package com.nateshao.web.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/10 10:35
  * @微信公众号 千羽的编程时光
@@ -8,5 +13,15 @@ package com.nateshao.web.interceptor;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
-public class WebMvcConfig {
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private BaseInterceptor baseInterceptor;
+
+    @Override
+    // 重写addInterceptors()方法，注册自定义拦截器
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(baseInterceptor);
+    }
 }
