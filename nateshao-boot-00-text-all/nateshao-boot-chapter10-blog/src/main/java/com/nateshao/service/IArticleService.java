810461a4a746d2a1af80b2d826c80f4b69be5c2f
nateshao <shaotongjie@gmail.com>
2020-11-16 08:43:52 +0800

    ArticleResponseData

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/service/IArticleService.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/service/IArticleService.java
@@ -1,5 +1,10 @@
 package com.nateshao.service;
 
+import com.github.pagehelper.PageInfo;
+import com.nateshao.model.domain.Article;
+
+import java.util.List;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/16 8:42
  * @微信公众号 千羽的编程时光
@@ -9,4 +14,21 @@ package com.nateshao.service;
  * @Gitee https://gitee.com/nateshao
  */
 public interface IArticleService {
+    // 分页查询文章列表
+    public PageInfo<Article> selectArticleWithPage(Integer page, Integer count);
+
+    // 统计前10的热度文章信息
+    public List<Article> getHeatArticles();
+
+    // 根据文章id查询单个文章详情
+    public Article selectArticleWithId(Integer id);
+
+    // 发布文章
+    public void publish(Article article);
+
+    // 根据主键更新文章
+    public void updateArticleWithId(Article article);
+
+    // 根据主键删除文章
+    public void deleteArticleWithId(int id);
 }
