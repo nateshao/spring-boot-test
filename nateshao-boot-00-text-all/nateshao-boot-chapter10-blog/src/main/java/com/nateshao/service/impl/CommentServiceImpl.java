373467e5d68998f80be1cab4f5afc76fa8921fd6
nateshao <shaotongjie@gmail.com>
2020-11-16 08:46:25 +0800

    ArticleResponseData

--- nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/service/impl/CommentServiceImpl.java
+++ nateshao-boot-00-text-all/nateshao-boot-chapter10-blog/src/main/java/com/nateshao/service/impl/CommentServiceImpl.java
@@ -1,5 +1,18 @@
 package com.nateshao.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.nateshao.dao.CommentMapper;
+import com.nateshao.dao.StatisticMapper;
+import com.nateshao.model.domain.Comment;
+import com.nateshao.model.domain.Statistic;
+import com.nateshao.service.ICommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/16 8:45
  * @微信公众号 千羽的编程时光
@@ -8,5 +21,33 @@ package com.nateshao.service.impl;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  */
-public class CommentServiceImpl {
+
+
+@Service
+@Transactional
+public class CommentServiceImpl implements ICommentService {
+    @Autowired
+    private CommentMapper commentMapper;
+    @Autowired
+    private StatisticMapper statisticMapper;
+
+    // 根据文章id分页查询评论
+    @Override
+    public PageInfo<Comment> getComments(Integer aid, int page, int count) {
+        PageHelper.startPage(page,count);
+        List<Comment> commentList = commentMapper.selectCommentWithPage(aid);
+        PageInfo<Comment> commentInfo = new PageInfo<>(commentList);
+        return commentInfo;
+    }
+
+    // 用户发表评论
+    @Override
+    public void pushComment(Comment comment){
+        commentMapper.pushComment(comment);
+        // 更新文章评论数据量
+        Statistic statistic = statisticMapper.selectStatisticWithArticleId(comment.getArticleId());
+        statistic.setCommentsNum(statistic.getCommentsNum()+1);
+        statisticMapper.updateArticleCommentsWithId(statistic);
+    }
+
 }
