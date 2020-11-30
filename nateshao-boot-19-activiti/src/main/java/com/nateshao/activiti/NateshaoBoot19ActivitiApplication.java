36f815607186bbb4de3e93b08e44d7c9a6c4435e
nateshao <shaotongjie@gmail.com>
2020-11-30 14:29:43 +0800

    优化代码

---
+++ nateshao-boot-19-activiti/src/main/java/com/nateshao/activiti/NateshaoBoot19ActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot19ActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot19ActivitiApplication.class, args);
+    }
+
+}
