098ef3e8f35668afb5f0bfcf146d20e0748b76a9
nateshao <shaotongjie@gmail.com>
2020-11-30 11:25:30 +0800

    初始化完成

---
+++ boot-nateshao-18-jdbctemplate/src/main/java/com/nateshao/BootNateshao18JdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNateshao18JdbctemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootNateshao18JdbctemplateApplication.class, args);
+    }
+
+}
---
+++ boot-nateshao-18-jdbctemplate/src/test/java/com/nateshao/BootNateshao18JdbctemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootNateshao18JdbctemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
