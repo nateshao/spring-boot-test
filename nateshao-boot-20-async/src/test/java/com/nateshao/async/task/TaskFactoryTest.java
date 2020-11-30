314236d53799cc5a2c5130e7a6119538a9d08d9a
nateshao <shaotongjie@gmail.com>
2020-11-30 19:52:58 +0800

    代码优化完成

---
+++ nateshao-boot-20-async/src/test/java/com/nateshao/async/task/TaskFactoryTest.java
@@ -0,0 +1,55 @@
+package com.nateshao.async.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/30 17:40
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description：测试任务
+ */
+@Slf4j
+class TaskFactoryTest {
+    @Autowired
+    private TaskFactory task;
+    /**
+     * 测试异步任务
+     */
+    @Test
+    public void asyncTaskTest() throws InterruptedException, ExecutionException {
+        long start = System.currentTimeMillis();
+        Future<Boolean> asyncTask1 = task.asyncTask1();
+        Future<Boolean> asyncTask2 = task.asyncTask2();
+        Future<Boolean> asyncTask3 = task.asyncTask3();
+
+        // 调用 get() 阻塞主线程
+        asyncTask1.get();
+        asyncTask2.get();
+        asyncTask3.get();
+        long end = System.currentTimeMillis();
+
+        log.info("异步任务全部执行结束，总耗时：{} 毫秒", (end - start));
+    }
+
+    /**
+     * 测试同步任务
+     */
+    @Test
+    public void taskTest() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        task.task1();
+        task.task2();
+        task.task3();
+        long end = System.currentTimeMillis();
+
+        log.info("同步任务全部执行结束，总耗时：{} 毫秒", (end - start));
+    }
+
+}
