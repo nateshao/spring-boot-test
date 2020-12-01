512605ff032ffc2d0afeabc7c3d25c406097b3b9
nateshao <shaotongjie@gmail.com>
2020-12-01 15:51:56 +0800

    代码优化完成

---
+++ nateshao-boot-21-cache-ehcache/src/main/java/com/nateshao/cache/ehcache/NateshaoBoot21CacheEhcacheApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao.cache.ehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoBoot21CacheEhcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoBoot21CacheEhcacheApplication.class, args);
+    }
+
+}
---
+++ nateshao-boot-21-cache-ehcache/src/test/java/com/nateshao/cache/ehcache/NateshaoBoot21CacheEhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.cache.ehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoBoot21CacheEhcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
