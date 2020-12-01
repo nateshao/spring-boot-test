6a39178321ae7619f110eee230df25a73eaee6c1
nateshao <shaotongjie@gmail.com>
2020-12-01 15:57:47 +0800

    代码优化完成

---
+++ nateshao-boot-21-cache-ehcache/src/main/java/com/nateshao/cache/ehcache/service/UserService.java
@@ -0,0 +1,37 @@
+package com.nateshao.cache.ehcache.service;
+
+import com.nateshao.cache.ehcache.entity.User;
+
+/**
+ * @date Created by 邵桐杰 on 2020/12/1 15:57
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description：
+ */
+public interface UserService {
+    /**
+     * 保存或修改用户
+     *
+     * @param user 用户对象
+     * @return 操作结果
+     */
+    User saveOrUpdate(User user);
+
+    /**
+     * 获取用户
+     *
+     * @param id key值
+     * @return 返回结果
+     */
+    User get(Long id);
+
+    /**
+     * 删除
+     *
+     * @param id key值
+     */
+    void delete(Long id);
+}
