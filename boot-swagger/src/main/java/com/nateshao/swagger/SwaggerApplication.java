310733760c6777fa96670bff012f9fb638bbc883
nateshao <shaotongjie@gmail.com>
2021-11-12 22:47:01 +0800

    swagger

---
+++ boot-swagger/src/main/java/com/nateshao/swagger/SwaggerApplication.java
@@ -0,0 +1,20 @@
+package com.nateshao.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/12 22:27
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+}
