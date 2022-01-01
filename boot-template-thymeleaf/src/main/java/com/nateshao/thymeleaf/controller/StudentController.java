cf47fef267b10d82cb00b9bccee5f9f44c36fd9f
nateshao <shaotongjie@gmail.com>
2022-01-01 15:01:27 +0800

    格式化代码

--- boot-template-thymeleaf/src/main/java/com/nateshao/thymeleaf/controller/StudentController.java
+++ boot-template-thymeleaf/src/main/java/com/nateshao/thymeleaf/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.nateshao.thymeleaf.controller;
 
 import com.nateshao.thymeleaf.service.StudentService;
 import com.nateshao.thymeleaf.vo.Student;
-import com.nateshao.thymeleaf.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,36 +29,5 @@ public class StudentController {
         model.addAttribute("studentList",studentList);
         return "studentIndex";
     }
-    // 进入添加界面
-    @RequestMapping("/intoAddStudent")
-    public String intoAdd(){
-        return "addStudent";
-    }
-    // 添加用户
-    @RequestMapping("/addStudent")
-    public String add(Student student){
-        studentService.addStudent(student);
-        return "redirect:/studentIndex";
-    }
 
-    @GetMapping("/findById/{stuNo}")
-    public String findById(Model model, @PathVariable("stuNo") Integer stuNo) {
-        model.addAttribute("student", studentService.findById(stuNo));
-        return "student";
-    }
-
-    @PostMapping("/addStudent")
-    public int addStudent(Student student) {
-        return studentService.addStudent(student);
-    }
-
-    @PutMapping("/updateStudent")
-    public int updateStudent(Student student) {
-        return studentService.updateStudent(student);
-    }
-
-    @DeleteMapping("/del/{stuNo}")
-    public int del(@PathVariable("stuNo") Integer stuNo) {
-        return studentService.del(stuNo);
-    }
 }
